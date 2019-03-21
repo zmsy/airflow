ffb6f2310ec1b8bb08e53204a57b1004a9e2ab9f
Zach Morrissey <zachmor05@gmail.com>
2019-03-21 08:28:13 -0700

    move start date for fantasy dag up

--- dags/fantasy_dag.py
+++ dags/fantasy_dag.py
@@ -13,7 +13,7 @@ default_args = {
     'email_on_failure': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2019, 3, 8)
+    'start_date': datetime(2019, 3, 20)
 }
 
 dag = airflow.DAG('fantasy_baseball', default_args=default_args)
