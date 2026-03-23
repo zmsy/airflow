bb2eac39e6875fc062e7d5cf9572a95715ec0cea
Zach Morrissey <zachmor05@gmail.com>
2026-03-22 20:21:08 -0700

    Fix postgres_ip

--- dags/lib/db.py
+++ dags/lib/db.py
@@ -7,6 +7,6 @@ import os
 # connection information for the database
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-POSTGRES_IP = "192.168.0.103"
+POSTGRES_IP = "192.168.0.14"
 POSTGRES_PORT = 5432
 POSTGRES_DB = "postgres"
